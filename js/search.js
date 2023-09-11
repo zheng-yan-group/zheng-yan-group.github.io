2238a06d6e1ec64e75de8de1104731340919c2df
zheng-yan-group <zhengyan_group@outlook.com>
2023-09-11 16:31:23 +0800

    Site updated: 2023-09-11 16:31:23

--- js/search.js
+++ js/search.js
@@ -1,92 +1,74 @@
-var searchFunc = function(path, search_id, content_id) {
-    'use strict';
-    $.ajax({
-        url: path,
-        dataType: "xml",
-        success: function( xmlResponse ) {
-            // get the contents from search data
-            var datas = $( "entry", xmlResponse ).map(function() {
-                return {
-                    title: $( "title", this ).text(),
-                    content: $("content",this).text(),
-                    url: $( "url" , this).text()
-                };
-            }).get();
+(() => {
+    var searchIndex = []
+    const searchForm = document.getElementById('search-form')
+    const searchBox = document.getElementById('searchbox')
+    const searchResults = document.getElementById('search-results')
 
-            var $input = document.getElementById(search_id);
-            var $resultContent = document.getElementById(content_id);
+    searchBox.select()
 
-            $input.addEventListener('input', function(){
-                var str='<ul class=\"search-result-list\">';                
-                var keywords = this.value.trim().toLowerCase().split(/[\s\-]+/);
-                $resultContent.innerHTML = "";
-                if (this.value.trim().length <= 0) {
-                    return;
-                }
-                // perform local searching
-                datas.forEach(function(data) {
-                    var isMatch = true;
-                    var content_index = [];
-                    var data_title = data.title.trim().toLowerCase();
-                    var data_content = data.content.trim().replace(/<[^>]+>/g,"").toLowerCase();
-                    var data_url = data.url;
-                    var index_title = -1;
-                    var index_content = -1;
-                    var first_occur = -1;
-                    // only match artiles with not empty titles and contents
-                    if(data_title != '' && data_content != '') {
-                        keywords.forEach(function(keyword, i) {
-                            index_title = data_title.indexOf(keyword);
-                            index_content = data_content.indexOf(keyword);
+    const doSearch = (keyword) => {
+        var results = []
+        var resultsEl = []
 
-                            if( index_title < 0 && index_content < 0 ){
-                                isMatch = false;
-                            } else {
-                                if (index_content < 0) {
-                                    index_content = 0;
-                                }
-                                if (i == 0) {
-                                    first_occur = index_content;
-                                }
-                            }
-                        });
-                    }
-                    // show search results
-                    if (isMatch) {
-                        str += "<li><a href='"+ data_url +"' class='search-result-title'>"+ data_title +"</a>";
-                        var content = data.content.trim().replace(/<[^>]+>/g,"");
-                        if (first_occur >= 0) {
-                            // cut out 100 characters
-                            var start = first_occur - 20;
-                            var end = first_occur + 80;
+        for (let i = 0; i < searchIndex.length; i++) {
+            const currentItem = searchIndex[i];
 
-                            if(start < 0){
-                                start = 0;
-                            }
-
-                            if(start == 0){
-                                end = 100;
-                            }
-                            if(end > content.length){
-                                end = content.length;
-                            }
-
-                            var match_content = content.substr(start, end); 
-
-                            // highlight all keywords
-                            keywords.forEach(function(keyword){
-                                var regS = new RegExp(keyword, "gi");
-                                match_content = match_content.replace(regS, "<span class=\"search-keyword\">"+keyword+"</span>");
-                            });
-                            
-                            str += "<p class=\"search-result\">" + match_content +"...</p>"
-                        }
-                        str += "</li>";
-                    }
-                });
-                str += "</ul>";
-                $resultContent.innerHTML = str;
-            });
+            if (JSON.stringify(currentItem).search(keyword) !== -1) {
+                results.push(currentItem)
+            }
         }
-    });
-}
+
+        if (results.length > 0) {
+            for (let i = 0; i < results.length; i++) {
+                const currentResult = results[i];
+                const currentResultEl = document.createElement('article')
+                currentResultEl.classList.add('post-list-item')
+                currentResultEl.innerHTML = `
+<a href="${currentResult.url}">
+    <div class="content">
+        ${(() => {
+                        if (currentResult.categories) {
+                            return `<div class="categories${document.body.attributes['data-uppercase-categories'].value ? ' text-uppercase' : ''}">${(() => {
+                                let categories = ''
+                                for (let i = 0; i < currentResult.categories.length; i++) {
+                                    const currentCategory = currentResult.categories[i]
+                                    categories += `<span>${currentCategory}</span>`
+                                }
+                                return categories
+                            })()}</div>`
+                        } else {
+                            return ''
+                        }
+                    })()}
+        <div class="title">${currentResult.title}</div>
+    </div>
+</a>
+                `
+                resultsEl.push(currentResultEl)
+            }
+        } else {
+            const el = document.createElement('div')
+            el.className = 'no-results'
+            el.innerHTML = 'No results found.'
+            resultsEl.push(el)
+        }
+
+        searchResults.innerHTML = ''
+        for (let i = 0; i < resultsEl.length; i++) {
+            const element = resultsEl[i];
+            searchResults.appendChild(element)
+        }
+    }
+
+    searchForm.addEventListener('submit', (ev) => {
+        ev.preventDefault()
+        if (searchIndex != '') {
+            doSearch(searchBox.value)
+        } else {
+            fetch(document.body.attributes['data-config-root'].value + document.body.attributes['data-search-path'].value).then(res => res.json()).then(data => {
+                searchIndex = data
+                doSearch(searchBox.value)
+            })
+        }
+    })
+})()
